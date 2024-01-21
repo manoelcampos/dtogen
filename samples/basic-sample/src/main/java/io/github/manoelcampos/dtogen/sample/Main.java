37497025380909bb61c4e58df670679bad1a510d
Manoel Campos <manoelcampos@gmail.com>
2024-01-21 18:04:56 -0300

    Update README
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- samples/basic-sample/src/main/java/io/github/manoelcampos/dtogen/sample/Main.java
+++ samples/basic-sample/src/main/java/io/github/manoelcampos/dtogen/sample/Main.java
@@ -6,7 +6,7 @@ package io.github.manoelcampos.dtogen.sample;
 public class Main {
     public static void main(String[] args) {
         final var country = new Country(1, "Brazil", "South America", 8_510_417_771L);
-        final var profession = new Profession(1, "Software Developer", "IT");
+        final var profession = new Profession(1, "Software Engineer", "IT");
         final var person = new Person(1, "Manoel Campos", 80, 42, "Brazil", country, profession);
         System.out.println(person);
         System.out.println();
